5f82dc29f17b802b8168e2210f4bf6c4a1f884f3
wenuja <wenuja7@gmail.com>
2026-04-20 00:06:38 +0530

    feat: initialize Grizzly HTTP server for Smart Campus application

---
+++ src/main/java/com/smartcampus/Main.java
@@ -0,0 +1,28 @@
+package com.smartcampus;
+
+import com.smartcampus.config.SmartCampusApplication;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+
+import java.io.IOException;
+import java.net.URI;
+
+public class Main {
+
+    public static final String BASE_URI = "http://localhost:8080/";
+
+    public static HttpServer startServer() {
+        return GrizzlyHttpServerFactory.createHttpServer(
+                URI.create(BASE_URI),
+                new SmartCampusApplication()
+        );
+    }
+
+    public static void main(String[] args) throws IOException {
+        HttpServer server = startServer();
+        System.out.println("Smart Campus API running at: " + BASE_URI + "api/v1");
+        System.out.println("Press Enter to stop the server...");
+        System.in.read();
+        server.shutdownNow();
+    }
+}
